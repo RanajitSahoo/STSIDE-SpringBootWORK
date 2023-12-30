fa17531188930c62b2f601a45ee155b10de573c3
RajaLucid <ranajitsahoo170597@gmail.com>
2023-12-31 01:11:34 +0530

    my first Spring boot project

---
+++ MyFirstSTSProject/src/main/java/com/jsp/sts/MyFirstStsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstStsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstStsProjectApplication.class, args);
+	}
+
+}
---
+++ MyFirstSTSProject/src/test/java/com/jsp/sts/MyFirstStsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstStsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
