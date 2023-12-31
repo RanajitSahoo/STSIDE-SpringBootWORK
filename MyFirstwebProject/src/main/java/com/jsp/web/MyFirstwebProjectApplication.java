447c68562fff725792ba3b8b20f1cadd0cae724e
RajaLucid <ranajitsahoo170597@gmail.com>
2023-12-31 19:59:55 +0530

    CRUD operation using spring

---
+++ MyFirstwebProject/src/main/java/com/jsp/web/MyFirstwebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstwebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstwebProjectApplication.class, args);
+	}
+
+}
